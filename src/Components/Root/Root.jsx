e4dfc0e358cb652b36f054e68e18e6d2b4ff675c
MirazZim <miraz.zim.38@gmail.com>
2025-01-11 12:59:03 +0600

    Did  nested Route and Navbar Setup. made a lot of Components. Did the Error Page thing. Created a Footer

--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -1,9 +1,21 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../Footer/Footer";
+import NavBar from "../NavBar/NavBar";
 
 
 const Root = () => {
     return (
         <div>
-            <h1>Hello From Root</h1>
+            {/* Egula Fix Thakbe */}
+            <NavBar></NavBar>
+            
+
+            {/* Majkhane ja Change Hobe shegula Outlet ey Thakbe */}
+            <Outlet></Outlet>
+
+
+            {/* Egula Fix Thakbe */}
+            <Footer></Footer>
         </div>
     );
 };
